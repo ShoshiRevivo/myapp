1e17e4b828fdaee0bc08bd2716d32d7ae94e248c
origin <shoshib1995@gmail.com>
2018-05-14 12:53:43 +0300

    add someone to log

---
+++ db/migrate/20180513142518_add_post_tble.rb
@@ -0,0 +1,8 @@
+class AddPostTble < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :body
+    end
+  end
+end
---
+++ db/migrate/20180513143006_add_index_to_posts_table.rb
@@ -0,0 +1,5 @@
+class AddIndexToPostsTable < ActiveRecord::Migration[5.2]
+  def change
+    add_index :posts, :title
+  end
+end
